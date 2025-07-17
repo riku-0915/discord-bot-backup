5dc2901125969edd1dc15e93892513500970ca78
riku-0915 <aianman915@icloud.com>
2025-07-18 00:54:08 +0900

    main2.py

--- main2.py
+++ main2.py
@@ -128,7 +128,7 @@ class AntiRaid(commands.Cog):
         embed = discord.Embed(
             title="🚨 自動モデレーション発動",
             description=f"{msg.author.mention} を **1時間タイムアウト** しました"
-理由 : **{reason}**",
+理由 : **{reason}**"""
             color=0xFF5555,
         )
         await msg.channel.send(embed=embed)
